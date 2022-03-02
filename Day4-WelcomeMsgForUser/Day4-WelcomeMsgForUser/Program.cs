4a2e8a20c3297c1e6e3024c0687832bc63e9ff6e
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-02 10:22:20 +0530

    Added welcome massage for user

---
+++ Day4-WelcomeMsgForUser/Day4-WelcomeMsgForUser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day4_WelcomeMsgForUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation program problem");
+        }
+    }
+}

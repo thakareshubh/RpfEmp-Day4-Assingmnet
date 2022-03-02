6b7656756e1d820c48bf1d18a3da7ddc69dbc661
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-02 10:25:50 +0530

    Added attendance of employee code

---
+++ Day4Emp-Uc1-AbsentPresent/Day4Emp-Uc1-AbsentPresent/ComputePresentAbsent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4Emp_Uc1_AbsentPresent
+{
+    public class ComputePresentAbsent
+    {
+        //variable initialization
+       public const int isPresent = 1;
+       public static Random random = new Random();
+       public static int check = random.Next(0, 2);
+
+        ///create selection methode to find present or absent
+       public void Compute()
+        {
+            if (check == isPresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not present");
+            }
+        }
+    }
+}
---
+++ Day4Emp-Uc1-AbsentPresent/Day4Emp-Uc1-AbsentPresent/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day4Emp_Uc1_AbsentPresent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputePresentAbsent p1 = new ComputePresentAbsent();
+            p1.Compute();
+        }
+    }
+}
